376d6a1855cf4ec93b98900aca03ac37300f0c5b
Bartosz <bartosiewicz-bartosz@wp.pl>
2021-11-22 12:47:29 +0100

    First commit - Spring

---
+++ Keyword-Spring/src/main/java/com/keyword/keywordspring/KeywordSpringApplication.java
@@ -0,0 +1,13 @@
+package com.keyword.keywordspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeywordSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeywordSpringApplication.class, args);
+    }
+
+}
---
+++ Keyword-Spring/src/test/java/com/keyword/keywordspring/KeywordSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyword.keywordspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeywordSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
